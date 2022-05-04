ffa19ad7e2b4d9daf9ba50e4ca30add6d6a3a734
infuman69 <saharoy.soham123@gmail.com>
2022-05-04 17:57:04 +0530

    added user detail style

--- src/Components/UserDetail/UserDetail.jsx
+++ src/Components/UserDetail/UserDetail.jsx
@@ -5,16 +5,31 @@ const UserDetail = () => {
   return (
     <div className={style.user_detail}>
       <h3 className={style.user_detail_head}>USER DETAILS</h3>
-      <div className="user-detail-flex">
-        <img src="" alt="" srcset="" />
-        <div className="user-name">
-          
+      <div className={style.user_detail_flex}>
+        <img src="https://avatars.dicebear.com/api/adventurer/Travis.Shanahan.svg" alt="" srcset="" className={style.user_detail_img}/>
+        <div className={style.user_name}>
+          Travis.Shanahan
         </div>
-        <div className="user-job-title">
-
+        <div className={style.user_bio}>
+          Sit ipsa nihil ratione.
         </div>
-        <div className="user-email">
-          
+        <div className={style.user_full_name}>
+          <label className={style.label}>Full Name</label>
+          <div className={style.text}>
+            Hans Conelly
+          </div>
+        </div>
+        <div className={style.user_job_title}>
+          <label className={style.label}>Job Title</label>
+          <div className={style.text}>
+            National Accountability Architecht
+          </div>
+        </div>
+        <div className={style.user_email}>
+          <label className={style.label}>Email</label>
+          <div className={style.text}>
+            Jena33@gmail.com
+          </div>
         </div>
       </div>
     </div>
